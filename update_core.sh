c7479664e1e8a908711c233b0d7a07b4dbc7287d
amerigena <amerigena@gmail.com>
2018-10-04 00:51:59 +0000

    Add files via upload

--- update_core.sh
+++ update_core.sh
@@ -2,10 +2,6 @@ sudo apt update && sudo apt install xfce4* gvfs* chromium-browser xpad
 
 wget -nv https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
-sudo apt update
-
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:stevenpusser.list"
-sudo apt update && sudo apt install palemoon
-
 sudo add-apt-repository ppa:papirus/papirus && sudo add-apt-repository ppa:claws-mail/ppa
-sudo apt update && sudo apt install papirus-icon-theme claws-mail
+sudo apt update && sudo apt install -y papirus-icon-theme claws-mail palemoon
