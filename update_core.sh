195d0f95df68284ebf1597243fd383f4a96d0e38
amerigena <amerigena@gmail.com>
2018-10-04 00:53:57 +0000

    Add files via upload

--- update_core.sh
+++ update_core.sh
@@ -1,5 +1,4 @@
 sudo apt update && sudo apt install xfce4* gvfs* chromium-browser xpad
-
 wget -nv https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:stevenpusser.list"
