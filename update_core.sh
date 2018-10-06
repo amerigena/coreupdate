fcdc60055dfbc9ebf0e3cab04d5f3df4d6925fa2
amerigena <amerigena@gmail.com>
2018-10-06 01:04:45 +0000

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt install xfce4* gvfs* chromium-browser xpad
+sudo apt update && sudo apt install xfce4* gvfs* chromium-browser xpad hexchat evince
 wget -nv https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:stevenpusser.list"
