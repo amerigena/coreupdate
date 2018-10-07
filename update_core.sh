3fda23dd12d3b26722f029bab201c9c006de3c47
amerigena <amerigena@gmail.com>
2018-10-07 23:01:19 +0000

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -3,5 +3,5 @@ sudo apt update && sudo apt install -y xfce4* gvfs* xpad hexchat evince
 wget -nv https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:stevenpusser.list"
-sudo add-apt-repository -y ppa:papirus/papirus && sudo add-apt-repository ppa:claws-mail/ppa
-sudo apt update && sudo apt install -y papirus-icon-theme claws-mail palemoon
+sudo add-apt-repository -y ppa:papirus/papirus && sudo add-apt-repository ppa:claws-mail/ppa && sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt update && sudo apt install -y papirus-icon-theme claws-mail palemoon libreoffice libreoffice-style*
