a1150c168fe8de9673dcf030a2a39471f0ba446f
amerigena <amerigena@gmail.com>
2018-10-12 10:59:35 +0000

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -5,4 +5,4 @@ wget -nv https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18
 sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:stevenpusser.list"
 sudo add-apt-repository -y ppa:papirus/papirus
-sudo apt update && sudo apt install -y papirus-icon-theme palemoon ice
+sudo apt update && sudo apt install -y papirus-icon-theme palemoon
