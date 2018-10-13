0f5c8445389e38bc35964ab05b6427bfaa93de87
amerigena <amerigena@gmail.com>
2018-10-13 01:19:18 +0000

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 # post- install script for Xubuntu Core 18.04
-sudo apt update && sudo apt install -y xfce4* gvfs* xpad hexchat evince git curl
+sudo apt update && sudo apt install -y xfce4* gvfs* xpad hexchat evince git curl gnumeric icedtea-8-plugin
 wget -nv https://download.opensuse.org/repositories/home:stevenpusser/xUbuntu_18.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/stevenpusser/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:stevenpusser.list"
