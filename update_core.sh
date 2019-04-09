0736251658090842f56f46e9b1d3562fd90a8b7a
amerigena <amerigena@gmail.com>
2019-04-09 19:33:22 -0400

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -8,4 +8,4 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 source /etc/os-release
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
 sudo add-apt-repository -y ppa:papirus/papirus
-sudo apt update && sudo apt install -y papirus-icon-theme palemoon brave-keyring brave-browser shellcheck
+sudo apt update && sudo apt install -y xfonts-terminus papirus-icon-theme palemoon brave-keyring brave-browser shellcheck
