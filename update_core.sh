ed33927bc9a3688e9026fe48126c8c564c64b7f3
amerigena <amerigena@gmail.com>
2020-05-05 06:56:47 -0400

    Update update_core.sh

--- update_core.sh
+++ update_core.sh
@@ -4,5 +4,5 @@ sudo apt update && sudo apt install -y xfce4* gvfs* xpad hexchat evince git curl
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 source /etc/os-release
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
-sudo add-apt-repository -y ppa:papirus/papirus
-sudo apt update && sudo apt install -y xfonts-terminus papirus-icon-theme palemoon brave-keyring brave-browser shellcheck
+sudo add-apt-repository -y ppa:papirus/papirus && sudo add-apt-repository -y ppa:i-nex-development-team/stable
+sudo apt update && sudo apt install -y xfonts-terminus papirus-icon-theme palemoon brave-keyring brave-browser shellcheck i-nex smartmontools gsmartcontrol
